549b1273e2bfcf5e46df355475a4384ec7e1c9ea
PrathyushaCherukuri <prathyushacherukuri011@gmail.com>
2025-12-29 10:06:08 +0000

    Keep only AWS content in aws branch

--- PYTHON/Logging_Example/src/main.py
+++
@@ -1,8 +0,0 @@
-from utils.logger import get_logger
-
-logger = get_logger("main")
-
-logger.info("Pipeline started")
-logger.info("Extracting data...")
-logger.error("Failed to download file")
-logger.info("Pipeline completed")
